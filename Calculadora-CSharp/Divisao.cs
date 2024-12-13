071925406c632805b66ea800f564b37e8062396f
Foqsz <contatovictorvinicius05@gmail.com>
2024-12-13 10:49:38 -0300

    feat: divisao

---
+++ Calculadora-CSharp/Divisao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_CSharp;
+
+public class Divisao
+{
+    public double SomarDivisao(double numeroA, double numeroB)
+    {
+        return numeroA / numeroB;
+    }
+
+}
+
